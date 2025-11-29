6e22ac55b69353aa801d3e76a80c0f455fda901a
adityakumarsandhaiya-collab <adityakumarsandhaiya@gmail.com>
2025-11-29 16:41:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const axios = require("axios");
 
 module.exports = async (req, res) => {
-  const number = req.query.number;
-  if (!number) {
-    return res.json({
+  try {
+    const number = req.query.number;
+
+    if (!number) {
+      return res.json({
+        status: false,
+        message: "Please provide ?number=",
+      });
+    }
+
+    const api = `https://ox-tawny.vercel.app/search_mobile?mobile=${number}&api_key=gavrawrand`;
+    const result = await axios.get(api);
+
+    res.json({
+      status: true,
+      number: number,
+      count: result.data.data.length || 0,
+      data: result.data.data,
+      developer: "Your Name",
+    });
+
+  } catch (err) {
+    res.json({
       status: false,
-      message: "Please provide ?number=",
+      message: "API error!",
+      error: err.message,
     });
   }
-
-  const api = `https://ox-tawny.vercel.app/search_mobile?mobile=${number}&api_key=gavrawrand`;
-  const result = await axios.get(api);
-
-  res.json({
-    status: true,
-    number: number,
-    data: result.data,
-    developer: "Your Name",
-  });
 };
